654c24dfdaeebb464fb7bd9b980d3c090613d7cb
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2025-09-23 14:42:29 +0200

    fix(app): ImageBackground

--- my-app/app/_layout.js
+++ my-app/app/_layout.js
@@ -1,41 +1,17 @@
 import { Stack } from "expo-router";
-import { ImageBackground, StyleSheet, View } from "react-native";
-
-const backgroundImage = require("../assets/images/bg.png");
 
 export default function RootLayout() {
   return (
-    <ImageBackground
-      source={backgroundImage}
-      style={styles.image}
-    >
-      <View style={{ flex: 1, backgroundColor: "red", borderBlockColor: "blue" }} >
-      <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="login"
-          options={{ title: "Se connecter", headerShown: false }}
-        />
-        <Stack.Screen
-          name="signup"
-          options={{ title: "S'inscrire", headerShown: false }}
-        />
-      </Stack>
-      </View>
-    </ImageBackground>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="login"
+        options={{ title: "Se connecter", headerShown: false }}
+      />
+      <Stack.Screen
+        name="signup"
+        options={{ title: "S'inscrire", headerShown: false }}
+      />
+    </Stack>
   );
 }
-
-const styles = StyleSheet.create({
-  image: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    resizeMode: "cover",
-  },
-});
-
-
