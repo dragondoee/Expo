1d7e8240648cb58bf54b81c46efab26dc8811402
Meylie <emilie.desgranges78@gmail.com>
2025-09-23 11:27:46 +0200

    Navbar

--- my-app/app/(app)/_layout.js
+++
@@ -1,8 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function RootLayout() {
-  return <Stack>
-      <Stack.Screen name="index" options={{title:"To-do List", headerShown:false}} />
-      <Stack.Screen name="profil" options={{title:"Profil", headerShown:false}} />
-    </Stack>;
-}
---
+++ my-app/app/(tabs)/_layout.js
@@ -0,0 +1,25 @@
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'To-do List',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profil"
+        options={{
+          title: 'Profil',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="account-circle" size={24} color="black" />,
+        }}
+      />
+    </Tabs>
+  );
+}
