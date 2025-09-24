71e37176e974d2d61903b42c1110e13f502ec066
dragondoee <trucmuchdoe@gmail.com>
2025-09-24 14:54:31 +0200

    api

---
+++ api/src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+require('./services/mongo');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
