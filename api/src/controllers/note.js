1adb8f3b22791a4829d8a32d4768fefcd2104e4d
Meylie <emilie.desgranges78@gmail.com>
2025-12-16 16:53:03 +0100

    fix time token

--- api/src/controllers/note.js
+++ api/src/controllers/note.js
@@ -8,7 +8,7 @@ const UserObject = require('../models/note');
 
 const config = require('../config');
 
-const JWT_MAX_AGE = "6m"; // 6 months
+const JWT_MAX_AGE = "100m"; // 100 minutes
 
 const SERVEUR_ERROR = 'SERVEUR_ERROR';
 
